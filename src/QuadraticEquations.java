28d73e3d23cd48046cbf7088a56ceb8b543d4c55
Eugene Bo <deafmuteman@gmail.com>
2017-07-16 16:05:27 +0300

    -исправил имя переменной root1 и root 2

--- src/QuadraticEquations.java
+++ src/QuadraticEquations.java
@@ -18,26 +18,39 @@ public class QuadraticEquations {
         System.out.print("c=");
         double c = sc.nextDouble();
 
-        System.out.format("\nYour equation is: %fx^2%+fx%+f=0", a, b, c);
+//        if (a == 0) {
+//            if (b != 0) {
+//
+//                System.out.format("\nYour equation has a linear type: %fx%+f=0", b, c);
+//                System.out.println("\nRoot: x=" + (-c / b));
+//            } else {
+//                System.out.print("No roots!");
+//            }
+//
+//        } else {
 
-        double d = (b * b) - (4 * a * c);
-        double Root1 = -b / (2 * a);
-        double Root2 = (Math.sqrt(Math.abs(d))) / (2 * a);
+            System.out.format("\nYour equation is: %fx^2%+fx%+f=0", a, b, c);
 
-        if (d >= 0) {
+            double d = (b * b) - (4 * a * c);
+            double root1 = -b / (2 * a);
+            double root2 = (Math.sqrt(Math.abs(d))) / (2 * a);
 
-            if (d == 0) {
+            if (d >= 0) {
 
-                System.out.println("\nRoots are: x1=x2=" + Root1);
+                if (d == 0) {
+
+                    System.out.println("\nRoots are: x1=x2=" + root1);
+                } else {
+                    System.out.print("\nRoots are: x1=" + (root1 + root2) + "; x2=" + (root1 - root2));
+                }
+
             } else {
-                System.out.print("\nRoots are: x1=" + (Root1 + Root2) + "; x2=" + (Root1 - Root2));
+
+                System.out.format("\nRoots are: x1=" + root1 + "+" + Math.abs(root2)+"*i" + "; x2=" + root1 + "-" + Math.abs(root2)+"*i");
             }
 
-        } else {
 
-            System.out.format("\nRoots are: x1=" + Root1 + "+" + Math.abs(Root2) + "*i" + "; x2=" + Root1 + "-" + Math.abs(Root2) + "*i");
-        }
-
+       // }
 
     }
 }
